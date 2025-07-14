0da11dc790bb5d64a01a3658f09fd0e004b3afd0
sahiladhikari39 <sahiladhi39@gmail.com>
2025-07-14 21:38:54 +0545

    learned to create django tables in postgresql

--- learn/home/models.py
+++ learn/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(max_length=2)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=10, default='Not Specified')
+    phone_number = models.CharField(max_length=10)
+    percentage = models.FloatField()
+    student_bio = models.TextField()
+    date_of_birth = models.DateField()
+    registration = models.DateTimeField()
