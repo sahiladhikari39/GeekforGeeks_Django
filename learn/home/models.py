49777ed53d914f386c0c9445e8706230f80f246c
sahiladhikari39 <sahiladhi39@gmail.com>
2025-07-15 18:13:56 +0545

    learned more about the fields

--- learn/home/models.py
+++ learn/home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField(max_length=2)
+    age = models.IntegerField()
     email = models.EmailField(unique=True)
     gender = models.CharField(max_length=10, default='Not Specified')
     phone_number = models.CharField(max_length=10)
@@ -11,3 +11,9 @@ class Student(models.Model):
     student_bio = models.TextField()
     date_of_birth = models.DateField()
     registration = models.DateTimeField()
+    # student_image = models.ImageField(upload_to= "images/students/")
+    # student_file = models.FileField(upload_to= "file/students/")
+    # created_at = models.DateTimeField(auto_now=True)
+    # updated_at = models.DateTimeField(auto_now_add=True)
+    # slug = models.SlugField()
+    # uid = models.UUIDField()
